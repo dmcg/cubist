fa87409eddc05e19c76847c167c808269e8cf937
Duncan McGregor <duncan@oneeyedmen.com>
2013-12-03 07:26:18 +0000

    Move layout into Positions

--- src/com/oneeyedmen/cubist/BorderLayoutPainter.java
+++ src/com/oneeyedmen/cubist/BorderLayoutPainter.java
@@ -6,27 +6,40 @@ import java.awt.geom.Dimension2D;
 public class BorderLayoutPainter implements Painter<Container> {
 
     public enum Position {
-        NORTH, SOUTH, EAST, WEST, CENTER
+        NORTH {
+            @Override
+            protected void paint(Paintable paintable, Graphics2D g, Dimension2D size) {
+                Dimension2D preferredSize = paintable.preferredSize(g);
+                double centerX = (size.getWidth() - preferredSize.getWidth()) / 2;
+                g.translate(centerX, 0);
+                paintable.paintOn(g, Dimensions.size(size.getWidth(), preferredSize.getHeight()));
+            }
+        },
+        SOUTH {
+            @Override
+            protected void paint(Paintable paintable, Graphics2D g, Dimension2D size) {
+                Dimension2D preferredSize = paintable.preferredSize(g);
+                double centerX = (size.getWidth() - preferredSize.getWidth()) / 2;
+                g.translate(centerX, size.getHeight() - preferredSize.getHeight());
+                paintable.paintOn(g, Dimensions.size(size.getWidth(), preferredSize.getHeight()));
+            }
+        };
+
+        protected void paint(Paintable paintable, Graphics2D g, Dimension2D size) {
+
+        }
     }
 
     @Override
     public void paint(Container container, Graphics2D g, Dimension2D size) {
         for (Paintable paintable : container.components()) {
-            Graphics2D newG = (Graphics2D) g.create();
             Object context = container.contextFor(paintable);
-            if (context == Position.NORTH) {
-                Dimension2D preferredSize = paintable.preferredSize(newG);
-                double centerX = (size.getWidth() - preferredSize.getWidth()) / 2;
-                newG.translate(centerX, 0);
-                paintable.paintOn(newG, Dimensions.size(size.getWidth(), preferredSize.getHeight()));
-            } else if (context == Position.SOUTH) {
-                Dimension2D preferredSize = paintable.preferredSize(newG);
-                double centerX = (size.getWidth() - preferredSize.getWidth()) / 2;
-                newG.translate(centerX, size.getHeight() - preferredSize.getHeight());
-                paintable.paintOn(newG, Dimensions.size(size.getWidth(), preferredSize.getHeight()));
-            }
+            if (!(context instanceof Position))
+                throw new IllegalStateException("Expected a Position");
+            ((Position) context).paint(paintable, (Graphics2D) g.create(), size);
         }
     }
+
 
     @Override
     public Dimension preferredSize(Container thing, Graphics2D g) {
