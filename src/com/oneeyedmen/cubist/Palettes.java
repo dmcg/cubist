f24b1f4dff8fa89cc57b96473bb77099fc059404
Duncan McGregor <duncan@oneeyedmen.com>
2013-12-16 07:41:36 +0000

    First rough cut at rendering a Guide

--- src/com/oneeyedmen/cubist/Palettes.java
+++ src/com/oneeyedmen/cubist/Palettes.java
@@ -4,8 +4,8 @@ import java.awt.*;
 
 public class Palettes {
 
-    private static final Font defaultFont = new Font("Lucida Grande", Font.PLAIN, 13);
-    private static final Palette DEFAULT = new SimplePalette(defaultFont, Color.BLACK, null);
+    public static final Font DEFAULT_FONT = new Font("Lucida Grande", Font.PLAIN, 13);
+    private static final Palette DEFAULT = new SimplePalette(DEFAULT_FONT, Color.BLACK, null);
 
     public static Palette defaultFor(Class<? extends Paintable> type) {
         return DEFAULT;
